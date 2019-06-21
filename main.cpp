9487276fec2aa81e1e696383f44288032454750d
whage <whage@freemail.hu>
2019-06-21 18:15:23 +0200

    Implement camera rotation. Needs some debugging

--- main.cpp
+++ main.cpp
@@ -47,14 +47,14 @@ int main() {
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    camera cam = *(new camera());
+    camera cam(vec3(-2,2,1), vec3(0,0,-1), vec3(0,1,0), 90, float(nx)/float(ny));
 
     hitable *list[5];
     list[0] = new sphere(vec3(0, 0, -1),    0.5, new lambertian(vec3(0.2, 0.2, 0.8)));
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.5, 0.5, 0.5), 0.05));
-    list[3] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.5, 0.3), 0.2));
-    list[4] = new sphere(vec3(0.5,0,0), 0.5, new dielectric(1.5));
+    list[2] = new sphere(vec3(-1,0,-1),     0.5, new metal(vec3(0.5, 0.5, 0.5), 0.05));
+    list[3] = new sphere(vec3(1,0,-1),      0.5, new metal(vec3(0.8, 0.5, 0.3), 0.2));
+    list[4] = new sphere(vec3(0.5,0,0),     0.5, new dielectric(1.5));
     hitable *world = new hitable_list(list, 5);
 
     for (int j = ny-1; j >= 0; j--) {
